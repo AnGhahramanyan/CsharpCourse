d062d623edee75a3afb274a46f0a5d9943779c83
AnGhahramanyan <Ghahramanyan1997@gmail.com>
2020-07-09 01:17:02 +0400

    First commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Transactions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("First number is  " + firstNumber);
+
+            Console.WriteLine("Enter an operator you want to use");
+            string c = Console.ReadLine();
+
+            Console.WriteLine("Enter the next number");
+            double nextNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Next number is  " + nextNumber);
+
+            double a=firstNumber;
+            double b=nextNumber;
+            string formula = "acb";
+            formula = formula.Replace("a", a.ToString()).Replace("b", b.ToString()).Replace("c", c.ToString());
+
+            var calc = new System.Data.DataTable();
+            Console.WriteLine(calc.Compute(formula, ""));
+
+        }
+    }
+}
